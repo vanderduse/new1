355c8f8ca30f06379a0320ebd23faead23e27278
Manish Patel <patelmanish839@gmail.com>
2024-03-22 17:07:22 +0530

    Add api integration

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 async function fetchGitHubProjects() {
-    const url = "https://api.github.com/users/YOUR_USERNAME/repos";
+    const n_latest_projects = 4;
+    const url = `http://localhost:8000/github-projects/?n_latest=${n_latest_projects}`;
     try {
         const response = await fetch(url);
         const result = await response.json();
@@ -23,3 +24,46 @@ async function fetchGitHubProjects() {
 }
 
 fetchGitHubProjects(); // Call the function to run it
+
+
+async function submitComment(name, comment) {
+    const url = 'http://localhost:8000/add-comment/';
+    const requestBody = {
+        added_by: name,
+        description: comment
+    };
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestBody)
+        });
+        if (response.ok) {
+            alert('Comment submitted successfully!');
+        } else {
+            throw new Error('Failed to submit comment');
+        }
+    } catch (error) {
+        console.error('Error submitting comment:', error);
+        alert('An error occurred while submitting the comment');
+    }
+}
+
+document.getElementById('comment-form').addEventListener('submit', function (event) {
+
+   console.log("herere")
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get the form data
+    const name = document.getElementById('comment-form-name').value;
+    const comment = document.getElementById('comment-form-description').value;
+
+    // Submit the comment
+    submitComment(name, comment);
+
+    // Reset the form fields
+    document.getElementById('comment-form-name').value = '';
+    document.getElementById('comment-form-description').value = '';
+});
